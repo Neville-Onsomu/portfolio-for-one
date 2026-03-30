c47bf551774c3843766f5f1cb425d3bd5425b85d
Neville-Onsomu <nevs.journal@gmail.com>
2026-03-29 19:44:51 -0500

    navbar done. what's left is the color palette at the end.

--- portfolio-for-one/app/navbar.tsx
+++ portfolio-for-one/app/navbar.tsx
@@ -1,11 +1,11 @@
 export default function NavBar() {
     return(
-        <div className="sticky top-2 h-[10vh] w-[40vw] m-[1vh] place-self-center grid grid-cols-5 place-items-center bg-red-900 rounded-4xl p-0">
-            <a href="" className="duration-300 ease-in-out hover:scale-110">Home</a>
-            <a href="" className="duration-300 ease-in-out hover:scale-110">Education</a>
-            <a href="" className="duration-300 ease-in-out hover:scale-110">Skills</a>
-            <a href="" className="duration-300 ease-in-out hover:scale-110">Projects</a>
-            <a href="" className="duration-300 ease-in-out hover:scale-110">Contact</a>
+        <div className="sticky top-2 h-[10vh] min-w-[40vw] m-[1vh] place-self-center grid grid-cols-5 place-items-center bg-white/50 backdrop-blur-xs rounded-4xl p-1">
+            <a href="" className="text-xl duration-300 hover:scale-110 active:scale-95">Home</a>
+            <a href="" className="text-xl duration-300 hover:scale-110 active:scale-95">Education</a>
+            <a href="" className="text-xl duration-300 hover:scale-110 active:scale-95">Skills</a>
+            <a href="" className="text-xl duration-300 hover:scale-110 active:scale-95">Projects</a>
+            <a href="" className="text-xl duration-300 hover:scale-110 active:scale-95">Contact</a>
         </div>
     )
 }
