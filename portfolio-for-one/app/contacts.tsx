adb781f95d0c8a9ca06d6a33183fb04b03a57ff9
Neville-Onsomu <nevs.journal@gmail.com>
2026-03-29 22:33:50 -0500

    contact section done

--- portfolio-for-one/app/contacts.tsx
+++ portfolio-for-one/app/contacts.tsx
@@ -1,32 +1,32 @@
 const contactLinks = [
     {
         name: "email",
-        handle: "andre",
+        handle: "your handle/link",
         href: "mailto:alex.johnson@email.com",
     },
     {
-        name: "linkedIn",
-        handle: "andre",
+        name: "linkedin",
+        handle: "your handle/link",
         href: "https://linkedin.com/in/alexjohnson",
     },
     {
-        name: "gitHub",
-        handle: "andre",
+        name: "github",
+        handle: "your handle/link",
         href: "https://github.com/alexjohnson",
     },
     {
         name: "twitter",
-        handle: "andre",
+        handle: "your handle/link",
         href: "https://twitter.com/alexjohnsondev",
     },
 ]
 
 export default function Contacts() {
     return (
-    <section id="contact" className="bg-slate-500">
+    <section id="contact" className="bg-slate-600/70 py-24">
         <div className="mx-auto justify-items-center">
-            <h2 className="text-center text-2xl font-semibold">Get In Touch</h2>
-            <p className="mt-2 text-center">my handles</p>
+            <h2 className="text-center text-5xl font-extrabold">get in touch</h2>
+            <p className="mt-2 text-lg text-center">my handles</p>
             
             <div className="mt-8 grid gap-6 grid-cols-2 justify-items-center">
                 {contactLinks.map((contact) => (
@@ -35,10 +35,10 @@ export default function Contacts() {
                     href={contact.href}
                     target={contact.name !== "Email" ? "_blank" : undefined}
                     rel={contact.name !== "Email" ? "noopener noreferrer" : undefined}
-                    className="justify-items-center border rounded-4xl p-4 w-full"
+                    className="justify-items-center bg-slate-600 rounded-4xl p-4 w-full hover:scale-110 duration-300"
                     >
-                        <p className="text-lg font-bold">[ {contact.name} ]</p>
-                        <p className="text-lg">{contact.handle}</p>
+                        <p className="text-3xl font-bold">[ {contact.name} ]</p>
+                        <p className="text-xl">{contact.handle}</p>
                     </a>
                 ))}
             </div>
